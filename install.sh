40ffa16615508d2da62cef6fcc7bba4b84d20cec
Magnus Hauge Bakke <magnus@idrift.no>
2015-10-24 17:20:19 +0200

    Add install files

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# Get current dir (so run this script from anywhere)
+export DOTFILES_DIR DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles itself first
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Create vim directory if it does not exist
+if [[ ! -e "~/.vim" ]]; then
+    mkdir "~/.vim"
+fi
+
+# Bunch of symlinks
+ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
+ln -sfv "$DOTFILES_DIR/runcom/.zshrc" ~
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/vim/vundle.vim" ~/.vim/
+ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
+ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
+
+# Install Vundle
+source "$DOTFILES_DIR/install/vundle.sh"
