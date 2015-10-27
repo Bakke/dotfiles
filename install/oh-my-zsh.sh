aeb779c4ec255a1b35a647c3c9777c89d7a77ac9
Magnus Hauge Bakke <magnus@idrift.no>
2015-10-27 09:56:24 +0100

    Fix oh-my-zsh install

--- install/oh-my-zsh.sh
+++ install/oh-my-zsh.sh
@@ -1,2 +1,4 @@
+ZSH_DIR=~/.oh-my-zsh
+
 # Install/update oh-my-zsh
-mkdir -p "$ZSH" && (git clone https://github.com/robbyrussell/oh-my-zsh.git "$ZSH" || (cd "$ZSH" && git pull origin master))
+mkdir -p "$ZSH_DIR" && (git clone https://github.com/robbyrussell/oh-my-zsh.git "$ZSH_DIR" || (cd "$ZSH_DIR" && git pull origin master))
