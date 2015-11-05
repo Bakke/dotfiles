ef28799365f5bc0526f0b79d74a7fefd07990684
Magnus Hauge Bakke <magnus@idrift.no>
2015-11-05 13:58:06 +0100

    Fix vundle install

--- install/vundle.sh
+++ install/vundle.sh
@@ -1,11 +1,9 @@
 BUNDLE_DIR=~/.vim/bundle/Vundle.vim
 
 # Install/update Vundle
-mkdir -p "$BUNDLE_DIR" && (git clone https://github.com/VundleVim/Vundle.vim.git "$BUNDLE_DIR" || (cd "$BUNDLE_DIR" && git pull origin master))
-
 if [[ ! -e "$BUNDLE_DIR" ]]; then
 	mkdir -p "$BUNDLE_DIR"
-	git clone git clone https://github.com/VundleVim/Vundle.vim.git "$BUNDLE_DIR"
+	git clone https://github.com/VundleVim/Vundle.vim.git "$BUNDLE_DIR"
 else
 	cd "$BUNDLE_DIR" && git pull origin master && cd -
 fi
