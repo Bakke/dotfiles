653482aa46216baad32c2b7da48c7abcda6da4ce
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2021-11-11 22:12:09 +0100

    Only install font if not present

--- install/linux.sh
+++ install/linux.sh
@@ -6,7 +6,9 @@ ln -sfv "$DOTFILES_DIR/rofi/config" ~/.config/rofi/
 . "$DOTFILES_DIR/install/nvim.sh"
 . "$DOTFILES_DIR/install/apt.sh"
 
-wget https://github.com/epk/SF-Mono-Nerd-Font/archive/refs/heads/master.zip
-7z x master.zip
-cd SF-Mono-Nerd-Font-master && mv *.otf ~/.local/share/fonts/
-cd .. && rm -rf master.zip SF-Mono-Nerd-Font-master
+if [[ ! -e "~/.local/share/fonts/SFMono Regular Nerd Font Complete.otf" ]]; then
+    wget https://github.com/epk/SF-Mono-Nerd-Font/archive/refs/heads/master.zip
+    7z x master.zip
+    cd SF-Mono-Nerd-Font-master && mv *.otf ~/.local/share/fonts/
+    cd .. && rm -rf master.zip SF-Mono-Nerd-Font-master
+fi
--- install/osx.sh
+++ install/osx.sh
@@ -33,7 +33,9 @@ if [ "$LARAVEL" = "y" ]; then
 	. "$DOTFILES_DIR/install/laravel-installer.sh"
 fi
 
-wget https://github.com/epk/SF-Mono-Nerd-Font/archive/refs/heads/master.zip
-7z x master.zip
-cd SF-Mono-Nerd-Font-master && mv *.otf ~/Library/Fonts/
-cd .. && rm -rf master.zip SF-Mono-Nerd-Font-master
+if [[ ! -e "~/Library/Fonts/SFMono Regular Nerd Font Complete.otf" ]]; then
+    wget https://github.com/epk/SF-Mono-Nerd-Font/archive/refs/heads/master.zip
+    unzip -a master.zip
+    cd SF-Mono-Nerd-Font-master && mv *.otf ~/Library/Fonts/
+    cd .. && rm -rf master.zip SF-Mono-Nerd-Font-master
+fi
