8491f7418e2acd4752674dc407ae6da223b7c947
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2021-12-15 00:16:30 +0100

    Update neovim for Linux

--- install/nvim.sh
+++ install/nvim.sh
@@ -1,3 +1,3 @@
-wget https://github.com/neovim/neovim/releases/download/v0.5.1/nvim.appimage
+wget https://github.com/neovim/neovim/releases/download/v0.6.0/nvim.appimage
 chmod u+x nvim.appimage
 sudo mv nvim.appimage /usr/bin/nvim
