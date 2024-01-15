92e964c1b5d45786397a153864d3f122d4326476
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2024-01-15 23:58:23 +0100

    Linux: Install Plymouth

--- install/yay.sh
+++ install/yay.sh
@@ -8,9 +8,14 @@ fi
 
 apps=(
     gnome-shell-extension-pop-shell
+    plymouth
+    plymouth-theme-arch-charge
     logiops
     # g810-led-git
     tidal-hifi-bin
 )
+
+# plymouth-set-default-theme -l
+# sudo plymouth-set-default-theme -R arch-charge
 
 yay -S --noconfirm "${apps[@]}"
