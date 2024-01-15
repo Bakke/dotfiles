0661bf04cb0f1eab85bebb3672f1a7a110db7727
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2024-01-15 22:44:14 +0100

    Linux: Only add icons and themes if they don't exist

--- install/linux/icons.sh
+++ install/linux/icons.sh
@@ -1,5 +1,7 @@
-git clone https://github.com/vinceliuice/Tela-icon-theme.git
-cd Tela-icon-theme
-./install.sh -a
-cd ..
-rm -rf Tela-icon-theme
+if [ ! -d "${HOME}/.local/share/icons/Tela" ]; then
+    git clone https://github.com/vinceliuice/Tela-icon-theme.git
+    cd Tela-icon-theme
+    ./install.sh -a
+    cd ..
+    rm -rf Tela-icon-theme
+fi
--- install/linux/themes.sh
+++ install/linux/themes.sh
@@ -1,5 +1,7 @@
-git clone https://github.com/vinceliuice/Orchis-theme.git
-cd Orchis-theme
-./install.sh
-cd ..
-rm -rf Orchis-theme
+if [ ! -d "${HOME}/.themes/Orchis" ]; then
+    git clone https://github.com/vinceliuice/Orchis-theme.git
+    cd Orchis-theme
+    ./install.sh
+    cd ..
+    rm -rf Orchis-theme
+fi
