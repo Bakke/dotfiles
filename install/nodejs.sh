cb78ea5039b524e2e4e08382dc1534cfc700b2e9
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2024-10-03 15:20:51 +0200

    Fix nodejs source config file

--- install/nodejs.sh
+++ install/nodejs.sh
@@ -1,5 +1,13 @@
 wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-source ~/.zshrc
+
+if [ "$ZSH_VERSION" ] && compctl; then # zsh
+    source ~/.zshrc
+elif [ "$BASH_VERSION" ] && complete; then # bash
+    source ~/.bashrc
+elif [ "$FISH_VERSION" ]; then # bash
+    source ~/.config/fish/config.fish
+fi
+
 nvm install --lts node
 npm install --global yarn
 
