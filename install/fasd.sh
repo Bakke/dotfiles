e6ac7b7e0009b1315a0fbabae3f25ddc0a65495a
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2024-10-03 17:31:10 +0200

    Move fasd files to config directory

--- install/fasd.sh
+++ install/fasd.sh
@@ -4,7 +4,7 @@ FASD_DIR=~/.fasd-git
 if [[ ! -e "$FASD_DIR" ]]; then
   mkdir -p "$FASD_DIR"
   git clone https://github.com/clvv/fasd.git "$FASD_DIR"
-  ln -sfv "$DOTFILES_DIR/fasd/.fasd-init" ~
+  ln -sfv "$DOTFILES_DIR/config/fasd/.fasd-init" ~
 else
   cd "$FASD_DIR" && git pull origin master && cd -
 fi
