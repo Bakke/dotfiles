bda569f2ed464727c9814254c9869de097c90f7f
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2024-10-03 17:29:23 +0200

    Move nvim files to config folder

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ ln -sfv "$DOTFILES_DIR/runcom/.zshrc" ~
 ln -sfv "$DOTFILES_DIR/runcom/.inputrc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 # ln -sfv "$DOTFILES_DIR/vim/.vimrc" ~
-ln -sfv "$DOTFILES_DIR/nvim/" ~/.config/
+ln -sfv "$DOTFILES_DIR/config/nvim/" ~/.config/
 ln -sfv "$DOTFILES_DIR/tmux/.tmux.conf" ~
 ln -sfv "$DOTFILES_DIR/config/alacritty/.alacritty.toml" ~
 
