a5a1c7d392fbd82691e918e1a2af11bdc1dc2840
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2024-10-05 22:38:50 +0200

    Change wallpaper

--- install/linux/sddm-theme.sh
+++ install/linux/sddm-theme.sh
@@ -13,7 +13,8 @@ rm -rf .theme_temp
 
 rm "${SDDM_THEMES_DIR}/${THEME_DIR}/theme.conf"
 cp "${DOTFILES_DIR}/config/sddm/theme.conf" "${SDDM_THEMES_DIR}/${THEME_DIR}/theme.conf"
-cp "${DOTFILES_DIR}/assets/wallpapers/hzmg6dgeja4d1.jpeg" "${SDDM_THEMES_DIR}/${THEME_DIR}/background.jpeg"
+# cp "${DOTFILES_DIR}/assets/wallpapers/hzmg6dgeja4d1.jpeg" "${SDDM_THEMES_DIR}/${THEME_DIR}/background.jpeg"
+cp "${DOTFILES_DIR}/assets/wallpapers/sunset.jpg" "${SDDM_THEMES_DIR}/${THEME_DIR}/sunset.jpg"
 
 sudo echo -e "[Theme]\nCurrent=${THEME_DIR}" > ${SDDM_CONFIG_PATH} || (echo -e "\e[31;02mFailed to write to ${SDDM_CONFIG_PATH}.\e[0m" && exit 1)
 
