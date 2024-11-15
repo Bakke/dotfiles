bc16a89a979bc7dc26cd3adb6234a492ee3fa85b
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2024-11-15 20:44:33 +0100

    Use common scripts for prev/next in i3/sway

--- config/i3/scripts/i3-next.sh
+++ config/i3/scripts/i3-next.sh
@@ -1,7 +1,16 @@
-wsNext=$(( $( i3-msg -t get_workspaces | jq '.[] | select(.focused).num' ) + $1))
+if [ -x "$(command -v swaymsg)" ]; then
+    cmdName='swaymsg'
+elif [ -x "$(command -v i3-msg)" ]; then
+    cmdName='i3-msg'
+else
+    echo 'Error: swaymsg or i3-msg is not installed.'
+    exit 1
+fi
+
+wsNext=$(( $( $cmdName -t get_workspaces | jq '.[] | select(.focused).num' ) + $1))
 
 if [ $wsNext -lt 1 ]; then
     wsNext=1
 fi
 
-i3-msg workspace number $wsNext
+$cmdName workspace number $wsNext
--- config/sway/scripts/sway-move-next.sh
+++
@@ -1,8 +0,0 @@
-wsNext=$(( $( swaymsg -t get_workspaces | jq '.[] | select(.focused).num' ) + $1))
-
-if [ $wsNext -lt 1 ]; then
-    wsNext=1
-fi
-
-swaymsg move container to workspace $wsNext
-swaymsg workspace $wsNext
