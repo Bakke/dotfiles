f90c36887bab22767cdd11ddeaf756f4e5ec3703
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2024-11-15 20:13:02 +0100

    i3 can't move below workspace 1

--- config/i3/scripts/i3-move-next.sh
+++ config/i3/scripts/i3-move-next.sh
@@ -1,3 +1,8 @@
 wsNext=$(( $( i3-msg -t get_workspaces | jq '.[] | select(.focused).num' ) + $1))
+
+if [ $wsNext -lt 1 ]; then
+    wsNext=1
+fi
+
 i3-msg move container to workspace $wsNext
 i3-msg workspace $wsNext
