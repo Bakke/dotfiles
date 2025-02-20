79c06a29b6d8234ed9f40f85ce4d4e7d27cba444
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2025-02-20 12:15:38 +0100

    Nvim lspconfig: Add languages to `ts_ls`

--- config/nvim/after/plugin/lspconfig.lua
+++ config/nvim/after/plugin/lspconfig.lua
@@ -118,7 +118,7 @@ local servers = {
                 {
                     name = '@vue/typescript-plugin',
                     location = vim.fn.stdpath 'data' .. '/mason/packages/vue-language-server/node_modules/@vue/language-server',
-                    languages = { 'vue' },
+                    languages = { 'vue', 'javascript', 'typescript' },
                 },
             },
         },
