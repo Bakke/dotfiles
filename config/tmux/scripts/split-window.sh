bf8071cca2d7f92fec0efbfed3802bf1ffa143dc
Magnus Hauge Bakke <magnus.bakke@ncl.no>
2025-02-20 12:25:20 +0100

    Tmux update `split-window` script

--- config/tmux/scripts/split-window.sh
+++ config/tmux/scripts/split-window.sh
@@ -1,8 +1,11 @@
 #!/bin/zsh
 tmux split-window -h -c "#{pane_current_path}"
+tmux send-keys C-i
 tmux split-window -v -c "#{pane_current_path}"
-tmux resize-pane -R 38
+tmux send-keys C-i
+tmux resize-pane -R 35
 tmux select-pane -t 1
 tmux new-window -c "#{pane_current_path}"
+tmux send-keys C-i
 tmux select-window -t 1
 # tmux send-keys 'vim' C-m
