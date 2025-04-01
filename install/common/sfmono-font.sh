a7a09b990fc09a45fec4a6c030dc7e6dc60e69f1
Magnus Hauge Bakke <mhbakke@proton.me>
2025-04-01 21:51:41 +0200

    Disable `sfmono-font.sh` installation on Arch Linux

--- install/common/sfmono-font.sh
+++ install/common/sfmono-font.sh
@@ -3,7 +3,7 @@
 if [[ ! -e "~/.local/share/fonts/SFMono Regular Nerd Font Complete.otf" ]]; then
     mkdir -p ~/.local/share/fonts
     wget https://github.com/epk/SF-Mono-Nerd-Font/archive/refs/heads/master.zip
-    7z x master.zip
+    unzip master.zip
     cd SF-Mono-Nerd-Font-master && mv *.otf ~/.local/share/fonts/
     cd .. && rm -rf master.zip SF-Mono-Nerd-Font-master
 fi
