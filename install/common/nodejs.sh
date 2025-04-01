e7a8263ea15be30344a2343565c084a5537aed22
Magnus Hauge Bakke <mhbakke@proton.me>
2025-04-01 21:09:37 +0200

    Update NVM

--- install/common/nodejs.sh
+++ install/common/nodejs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 
 if [ "$ZSH_VERSION" ] && compctl; then # zsh
     source ~/.zshrc
