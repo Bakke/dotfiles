b914e4f17ee5598937515bff7f873129cfb39c39
Magnus Hauge Bakke <mhbakke@proton.me>
2025-09-05 19:41:02 +0200

    `split-window-vim.sh` remove comment

--- config/tmux/scripts/split-window-vim.sh
+++ config/tmux/scripts/split-window-vim.sh
@@ -4,6 +4,5 @@ DEFAULT="3"
 SPLITS="${1:-${DEFAULT}}"
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# . ~/.dotfiles/config/tmux/scripts/split-window.sh "${SPLITS}"
 . "${CURRENT_DIR}/split-window.sh" "${SPLITS}"
 tmux send-keys 'vim' C-m
