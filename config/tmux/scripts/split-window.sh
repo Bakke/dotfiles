19b3099adef1ef6cb657b2fdd367654422a29c3b
Magnus Hauge Bakke <mhbakke@proton.me>
2026-01-30 10:00:52 +0100

    Tmux split window script has default of 2 panes

--- config/tmux/scripts/split-window.sh
+++ config/tmux/scripts/split-window.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-DEFAULT="3"
+DEFAULT="2"
 SPLITS="${1:-${DEFAULT}}"
 START_COLUMNS=$(tput cols)
 
 tmux split-window -h -c "#{pane_current_path}"
 tmux send-keys C-l
 
-if [[ "${SPLITS}" == "${DEFAULT}" ]]; then
-    tmux split-window -v -c "#{pane_current_path}"
-    tmux send-keys C-l
-fi
+# if [[ "${SPLITS}" == "${DEFAULT}" ]]; then
+#     tmux split-window -v -c "#{pane_current_path}"
+#     tmux send-keys C-l
+# fi
 
 if [[ ${START_COLUMNS} -ge 230 ]]; then
     tmux resize-pane -R 35
