33d08b0d6d26713baea973aa8c66944210d98934
Magnus Hauge Bakke <mhbakke@proton.me>
2026-04-13 13:14:45 +0200

    Fix `sh` syntax for `.env` files

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -1,15 +1,2 @@
 require("core.options")
 require("core.lsp")
-
-vim.filetype.add({
-    extension = {
-        env = "sh", -- Set .env files to use shell syntax
-    },
-    filename = {
-        [".env"] = "sh",
-        ["env"] = "sh",
-    },
-    pattern = {
-        ["%.env%.[%w%.]+"] = "sh", -- Matches .env.local, .env.development, etc.
-    },
-})
