e2de831062a14c40547081d5c137315d2740a3a0
Magnus Hauge Bakke <mhbakke@proton.me>
2026-06-19 09:21:50 +0200

    Add `diffview.nvim` keybindings inside the diffview

--- config/nvim/after/plugin/diffview.lua
+++ config/nvim/after/plugin/diffview.lua
@@ -1,3 +1,16 @@
+require("diffview").setup({
+    keymaps = {
+        -- Keymaps active inside the side-by-side diff buffers
+        view = {
+            { "n", "cc", "<cmd>Git commit<cr>", { desc = "Git commit via Fugitive" } },
+        },
+        -- Keymaps active inside the file panel list sidebar
+        file_panel = {
+            { "n", "cc", "<cmd>Git commit<cr>", { desc = "Git commit via Fugitive" } },
+        },
+    }
+})
+
 vim.keymap.set('n', '<leader>gd', function()
     local lib = require("diffview.lib")
     local view = lib.get_current_view()
